2319ec7b4db8f967580044be07f0b24c1d18e88b
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-11-26 21:44:40 -0800

    first commit

---
+++ photoframegtk.py
@@ -0,0 +1,56 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, GObject, GLib
+import glob
+
+# Define the list of images
+
+images = glob.glob('/home/charliepi/Pictures/converted/*.jpg')
+# Initialize GTK
+Gtk.init()
+
+# Create a new window
+window = Gtk.Window()
+
+# Create an image widget
+image = Gtk.Image()
+window.add(image)
+
+# Set the initial image
+image.set_from_file(images[0])
+current_index = 0
+
+def show_next_image():
+    global current_index  # Add this line to access the global variable
+
+    print("this is idx {}".format(current_index))
+    print("this is len images {}".format(len(images)))
+
+    # Get the next image index
+    if current_index + 1 < len(images):
+        current_index = current_index + 1  # Assign the value of idx + 1 to current_index
+        next_index = current_index
+        image.set_from_file(images[next_index])
+        GLib.timeout_add(10000, show_next_image)
+
+    else:
+        # Reset to the first image
+        current_index = 0
+        print(current_index)
+        next_index = current_index
+        print(images[next_index])
+        image.set_from_file(images[next_index])
+        GLib.timeout_add(10000, show_next_image)
+
+# Start the timer for the first image
+
+GLib.timeout_add(10000, show_next_image)
+
+# Connect the window's destroy event to quit GTK
+window.connect('destroy', Gtk.main_quit)
+
+# Show the window
+window.show_all()
+
+# Start the GTK main loop
+Gtk.main()
---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+sudo apt update;
+sudo apt dist-upgrade;
+sudo apt install libgtk-4-dev -y;
+
+
+CWD="/home/pipi/photo_frame_gtk/"
+SERVICE_DIR="/etc/systemd/system/"
+SERVICE_FILE="/home/pipi/photo_frame_gtk/photoframegtk.service"
+BIN_DIR="/usr/bin/"
+BIN_FILE="/usr/bin/photoframegtk.py"
+SRC_FILE="/home/pipi/photo_frame_gtk/photoframegtk.py"
+
+cd $CWD;
+sudo mv $SERVICE_FILE $SERVICE_DIR;
+sudo mv $SRC_FILE $BIN_FILE;
+sudo chmod +rx $BIN_FILE;
+sudo chown root:root $BIN_FILE;
+sudo systemctl daemon-reload;
+sudo systemctl enable photoframegtk.service;
+sudo systemctl start photoframegtk.service;
+sudo systemctl status photoframegtk.service;
+

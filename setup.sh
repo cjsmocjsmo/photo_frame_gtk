98972a83f38ef5e0a79380ca1921c057d7dce0af
Charlie J Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-12-11 00:27:02 -0800

    Update image paths and timer duration

--- setup.sh
+++ setup.sh
@@ -1,26 +1,3 @@
 #!/bin/sh
 
-# sudo apt update;
-# sudo apt dist-upgrade;
-# sudo apt install python3-gi python3-gi-cairo gir1.2-gtk-4.0 -y;
-
-
-# CWD="/home/pipi/photo_frame_gtk/"
-# SERVICE_DIR="/etc/systemd/system/"
-# SERVICE_FILE="/home/pipi/photo_frame_gtk/photoframegtk.service"
-# BIN_DIR="/usr/bin/"
-BIN_FILE="/usr/bin/photoframegtk.py"
-SRC_FILE="/home/pipi/photo_frame_gtk/photoframegtk.py"
-
-# cd $CWD;
-# sudo mv $SERVICE_FILE $SERVICE_DIR;
-sudo mv $SRC_FILE $BIN_FILE;
-sudo chmod +rx $BIN_FILE;
-sudo chown root:root $BIN_FILE;
-# sudo systemctl daemon-reload;
-# sudo systemctl enable photoframegtk.service;
-# sudo systemctl start photoframegtk.service;
-# sudo systemctl status photoframegtk.service;
-
-python3 $BIN_FILE;
-
+DISPLAY=:0 nohup python3 photoframegtk.py

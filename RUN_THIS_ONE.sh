143375f78f003878e8ecc63c7e55cbb31b67fd37
Charlie_Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2024-01-01 23:27:50 -0800

    Update README and RUN_THIS_ONE.sh files

--- RUN_THIS_ONE.sh
+++ RUN_THIS_ONE.sh
@@ -4,6 +4,3 @@
 sudo cp -pvr photo-frame-gtk-autostart.desktop /etc/xdg/autostart/;
 sudo chown root:root /etc/xdg/autostart/photo-frame-gtk-autostart.desktop;
 sudo reboot;
-
-
-# DISPLAY=:0 nohup python3 photoframegtk.py

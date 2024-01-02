989addb29f01e0945c27abaeec6e93d5db9d3898
Charlie_Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2024-01-01 23:18:08 -0800

    Add README and update photo frame script
    Delete unnecessary start script
    
    Update photo frame script to display images in full screen

--- photo_frame_gtk_start.sh
+++
@@ -1,3 +0,0 @@
-cd /home/hpi/photo_frame_gtk;
-
-DISPLAY=:0 nohup /usr/bin/python3 photoframegtk.py
--- photoframegtk.py
+++ photoframegtk.py
@@ -4,10 +4,8 @@ from gi.repository import Gtk, GObject, GLib, Gdk
 import glob
 
 # Define the list of images
-
 images = glob.glob('/home/hpi/Pictures/photo_frame_images/*.jpg')
-# images = glob.glob('/home/hpi/Pictures/converted/*.jpg')
-# images = glob.glob('/media/hpii/F3FB-CFFB/Master_Master/*.jpg')
+
 # Initialize GTK
 Gtk.init()
 
@@ -20,17 +18,19 @@ window.add(image)
 
 # Set the initial image
 image.set_from_file(images[0])
-# current_index = 0
+
+# Set background color to black
 color = Gdk.Color(red=0, green=0, blue=0)
 window.modify_bg(Gtk.StateType.NORMAL, color)
+
+# Set the window to fullscreen
 window.fullscreen()
+
+# Set current index to 0
 current_index = 0
 
 def show_next_image():
-    global current_index  # Add this line to access the global variable
-
-    print("this is idx {}".format(current_index))
-    print("this is len images {}".format(len(images)))
+    global current_index
 
     # Get the next image index
     if current_index + 1 < len(images):
@@ -42,14 +42,11 @@ def show_next_image():
     else:
         # Reset to the first image
         current_index = 0
-        print(current_index)
         next_index = current_index
-        print(images[next_index])
         image.set_from_file(images[next_index])
         GLib.timeout_add(20000, show_next_image)
 
 # Start the timer for the first image
-
 GLib.timeout_add(20000, show_next_image)
 
 # Connect the window's destroy event to quit GTK
